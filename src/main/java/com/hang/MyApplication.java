52fd774bdba519afd11dd8c51ad1d20c405bbf72
hang1 <hang14756@163.com>
2023-07-05 20:00:23 +0800

    今日份的学习

--- src/main/java/com/hang/MyApplication.java
+++ src/main/java/com/hang/MyApplication.java
@@ -1,8 +1,13 @@
 package com.hang;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +21,8 @@ public class MyApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class,args);
+        SpringApplication springApplication = new SpringApplication(MyApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
     }
 }
--- src/test/java/com/hang/SpringTest.java
+++ src/test/java/com/hang/SpringTest.java
@@ -3,15 +3,23 @@ package com.hang;
 import com.hang.service.MyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.AvailabilityState;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.event.ApplicationEventMulticaster;
 
 @SpringBootTest
 public class SpringTest {
     @Autowired
     private MyService myService;
 
+    @Autowired
+    private ApplicationAvailability availability;
     @Test
     public void test(){
         System.out.println("hello==================================");
+        System.out.println(availability.getLivenessState());//生存状态
+        System.out.println(availability.getReadinessState());//是否可继续接受
     }
 }
